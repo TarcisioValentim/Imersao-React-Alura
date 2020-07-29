3d368269842321f45284f495c02a315249c1264e
Tarcisio Valentim <33500059+TarcisioValentim@users.noreply.github.com>
2020-07-29 09:47:17 -0300

    up index.js
    alt href para link do desenvolvedor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://tarcisiovalentim.github.io/meuportifolio/">
+        <img src="https://fontmeme.com/permalink/200729/1583a07a5dfde28037bb1706733e58ce.png" alt="Logo webflix" />
       </a>
       <p>
         Orgulhosamente criado durante a
